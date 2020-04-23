b29d489ceb30f63584e3f890178ca528691cc9ed
Karina Tarabarina <k.tarabarina@superjob.ru>
2020-04-23 15:26:26 +0300

    Небольшие правки

--- tests/dropdown_list_test.js
+++ tests/dropdown_list_test.js
@@ -5,11 +5,11 @@ Feature('Выпадающий список');
 Scenario('Выбираем опцию Option 1',  () => {
     I.amOnPage('https://the-internet.herokuapp.com/dropdown');
     I.selectOption('#dropdown', 'Option 1' );
-    //I.waitForValue()
+    I.waitForValue('#dropdown', '1')
 });
 
-Scenario('Выбираем опцию Option 2',  () => {
+Scenario('Выбираем опцию Option 2 ',  () => {
     I.amOnPage('https://the-internet.herokuapp.com/dropdown');
     I.selectOption('#dropdown', '2' );
-    //I.waitForValue()
+    I.waitForText('Option 2', 30, '#dropdown')
 });
